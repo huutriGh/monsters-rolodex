0afc94c59190b980d20d0de1b83b358bc4453025
unknown <huutriqt13@hotmail.com>
2019-12-11 16:00:58 +0700

    Updates

---
+++ precache-manifest.cb0c8b6df6fba30d49623ea17d6c3ab0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be35b82a271e62214af1afee3b4e1e8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cb3bd0e74f0a8ed4ab08",
+    "url": "/monsters-rolodex/static/css/main.a2b7fb9b.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb3bd0e74f0a8ed4ab08",
+    "url": "/monsters-rolodex/static/js/main.c3ac503c.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
